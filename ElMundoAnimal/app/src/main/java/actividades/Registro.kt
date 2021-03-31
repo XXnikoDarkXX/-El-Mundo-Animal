6973ef7df0d49a361630e9aec8ea6c76aaa08303
XXnikoDarkXX <nicocheto1@gmail.com>
2021-03-31 22:34:00 +0200

    Terminado layout y diseño de Registro
    He creado el layout con el diseño del registro

---
+++ ElMundoAnimal/app/src/main/java/actividades/Registro.kt
@@ -0,0 +1,12 @@
+package actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nicolasfernandez.elmundoanimal.R
+
+class Registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
--- ElMundoAnimal/app/src/main/java/com/nicolasfernandez/elmundoanimal/MainActivity.kt
+++ ElMundoAnimal/app/src/main/java/com/nicolasfernandez/elmundoanimal/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.nicolasfernandez.elmundoanimal
 
+import actividades.Registro
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun irRegistro(view: View) {
+
+
+        startActivity(Intent(this, Registro::class.java))
+
     }
 }
