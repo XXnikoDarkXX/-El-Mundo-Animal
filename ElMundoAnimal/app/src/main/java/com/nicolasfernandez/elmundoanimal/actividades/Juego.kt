8a1d9bae8d907b07dd0b5967a8c1fb8e8ae1a042
XXnikoDarkXX <nicocheto1@gmail.com>
2021-05-12 21:41:49 +0200

    Diseñando Juego
    -Diseñando y maquentando la parte de Trivia (Juego)

---
+++ ElMundoAnimal/app/src/main/java/com/nicolasfernandez/elmundoanimal/actividades/Juego.kt
@@ -0,0 +1,12 @@
+package com.nicolasfernandez.elmundoanimal.actividades
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nicolasfernandez.elmundoanimal.R
+
+class Juego : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_juego)
+    }
+}
