5172336010694728a98f818d49d47eaa235f7962
XXnikoDarkXX <nicocheto1@gmail.com>
2021-05-27 19:16:12 +0200

    Implementacion de puntuacion de jugador en el juego
    - Por cada pregunta correcta se obtiene 5 puntos, y al acabar la partida se le metera en la puntuacion del jugador para el ranking

--- ElMundoAnimal/app/src/main/java/com/nicolasfernandez/elmundoanimal/actividades/Juego.kt
+++ ElMundoAnimal/app/src/main/java/com/nicolasfernandez/elmundoanimal/actividades/Juego.kt
@@ -1,5 +1,7 @@
 package com.nicolasfernandez.elmundoanimal.actividades
 
+import android.content.Intent
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,7 +9,9 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.OnCompleteListener
 import com.nicolasfernandez.elmundoanimal.R
 import com.nicolasfernandez.elmundoanimal.clases.Animal
 import com.nicolasfernandez.elmundoanimal.clases.Pregunta
@@ -19,6 +23,9 @@ import java.util.Random
 class Juego : AppCompatActivity() {
     val txtVida: TextView by lazy { findViewById<TextView>(R.id.txtVida) }
     val txtIdPregunta: TextView by lazy { findViewById<TextView>(R.id.txtIdPregunta) }
+
+    val txtPuntuacionJugador: TextView by lazy { findViewById<TextView>(R.id.txtPuntuacionJugador) }
+
     val txtPregunta: TextView by lazy { findViewById(R.id.txtPregunta) }
     var contador: Int = 3
     val btnA: Button by lazy { findViewById<Button>(R.id.btnA) }
@@ -28,13 +35,15 @@ class Juego : AppCompatActivity() {
     val imgAnimal: ImageView by lazy { findViewById(R.id.imgAnimal) }
     var preguntasTotales: ArrayList<Pregunta> = ArrayList<Pregunta>()
     val preguntasFinalizadas: ArrayList<Pregunta> = ArrayList<Pregunta>()
+    var puntuacion:Int = 0
+    lateinit var jugador:Usuario
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_juego)
-
+        txtPuntuacionJugador.text="Puntuacion = 0"
 
         txtVida.text = txtVida.text.toString() + " " + contador
-
+      puntuacionJugador()
         //iniciamos el juego
         //    iniciarJuego()
 
@@ -43,74 +52,23 @@ class Juego : AppCompatActivity() {
 
 
         btnA.setOnClickListener {
-
-            var preguntas = preguntasTotales
-            Toast.makeText(this, "" + preguntasTotales.size, Toast.LENGTH_LONG).show()
-            var comprobacion:Boolean=false
-            for (preg in preguntas) {
-                if (this.txtIdPregunta.text.toString().equals(preg.id)) {
-                    if (preg.correcto.equals(btnA.text.toString())) {
-                        comprobacion = true
-                    }
-                }
-            }
-
-            if (comprobacion){
-                comprobarPreguntas(preguntas)
-
-            }else{
-                contador--
-                txtVida.text ="Total de vidas:" + " " + contador
-                Toast.makeText(this,"Fallastes ",Toast.LENGTH_LONG).show()
-            }
+            clickearBoton(btnA)
         }
 
         btnB.setOnClickListener {
-            var preguntas = preguntasTotales
-            Toast.makeText(this, "" + preguntasTotales.size, Toast.LENGTH_LONG).show()
-            var comprobacion:Boolean=false
-            for (preg in preguntas) {
-                if (this.txtIdPregunta.text.toString().equals(preg.id)) {
-                    if (preg.correcto.equals(btnB.text.toString())) {
-                        comprobacion = true
-                    }
-                }
-            }
+            clickearBoton(btnB)
 
-            if (comprobacion){
-                comprobarPreguntas(preguntas)
-
-            }else{
-                contador--
-                txtVida.text ="Total de vidas:"+ " " + contador
-                Toast.makeText(this,"Fallastes ",Toast.LENGTH_LONG).show()
-            }
         }
 
         btnC.setOnClickListener {
-            var preguntas = preguntasTotales
-            Toast.makeText(this, "" + preguntasTotales.size, Toast.LENGTH_LONG).show()
-            var comprobacion:Boolean=false
-            for (preg in preguntas) {
-                if (this.txtIdPregunta.text.toString().equals(preg.id)) {
-                    if (preg.correcto.equals(btnC.text.toString())) {
-                        comprobacion = true
-                    }
-                }
-            }
+            clickearBoton(btnC)
 
-            if (comprobacion){
-                comprobarPreguntas(preguntas)
 
-            }else{
-                contador--
-                txtVida.text ="Total de vidas:"+ " " + contador
-                Toast.makeText(this,"Fallastes ",Toast.LENGTH_LONG).show()
-            }
         }
 
         btnD.setOnClickListener {
-            var preguntas = preguntasTotales
+            clickearBoton(btnD)
+            /*var preguntas = preguntasTotales
             Toast.makeText(this, "" + preguntasTotales.size, Toast.LENGTH_LONG).show()
             var comprobacion:Boolean=false
             for (preg in preguntas) {
@@ -128,7 +86,8 @@ class Juego : AppCompatActivity() {
                 contador--
                 txtVida.text ="Total de vidas:"+ " " + contador
                 Toast.makeText(this,"Fallastes ",Toast.LENGTH_LONG).show()
-            }
+            }*/
+
         }
 
 
@@ -159,7 +118,6 @@ class Juego : AppCompatActivity() {
     }
 */
     fun obtenerPreguntas() {
-
         Database.firebaseDB.collection("juego").get().addOnSuccessListener { result ->
             val arrrayPreguntas: ArrayList<Pregunta> = ArrayList<Pregunta>()
             for (document in result) {
@@ -181,8 +139,12 @@ class Juego : AppCompatActivity() {
 
     }
 
+    /**
+     * Funcion pora poner las preguntas aleatorias de la colecion de preguntas
+     * @param preguntas: ArrayList con las preguntas de la bbdd
+     */
     fun ponerPreguntaAleatoria(preguntas: ArrayList<Pregunta>) {
-        if (preguntas.size>0) {
+        if (preguntas.size > 0) {
             var indice = rand(0, preguntas.size - 1)
 
             //Damos valores
@@ -198,8 +160,8 @@ class Juego : AppCompatActivity() {
             btnB.text = preguntas.get(indice).b
             btnC.text = preguntas.get(indice).c
             btnD.text = preguntas.get(indice).d
-        }else{
-            Toast.makeText(this,"Ganastes el juego",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "Ganastes el juego", Toast.LENGTH_LONG).show()
         }
 
     }
@@ -210,6 +172,10 @@ class Juego : AppCompatActivity() {
         return (start..end).random()
     }
 
+    /**
+     * Funcion para eliminar las preguntas ya contestadas durante el transcurso del juego
+     * @param preguntas: Coleccion con las preguntas de la bbddd
+     */
     fun comprobarPreguntas(preguntas: ArrayList<Pregunta>) {
 
         var preguntasRestantes: ArrayList<Pregunta> = preguntas
@@ -220,23 +186,108 @@ class Juego : AppCompatActivity() {
                 preguntasRestantes.remove(pregunta)
             }
 
-/*
-            for (i in 0..this.preguntasFinalizadas.size) {
-                for (pregunta in preguntas) {
-                    if (preguntasFinalizadas.get(i).id.equals(pregunta.id)) {
-
-                        preguntasRestantes.remove(pregunta)
-
-                    }
-                }
-
-            }
-
-            */
         }
 
         ponerPreguntaAleatoria(preguntasRestantes)
     }
+
+    /**
+     * Funcion para que cuando clicke
+     */
+    fun clickearBoton(boton: Button) {
+
+        if (this.contador == 0) {
+            terminarJuego()
+
+
+        } else {
+
+
+            var preguntas = preguntasTotales
+            var comprobacion: Boolean = false
+            for (preg in preguntas) {
+                if (this.txtIdPregunta.text.toString().equals(preg.id)) {//Si la id de la pregunta actual es la misma que la encontrada
+                    //en la coleccion
+                    if (preg.correcto.equals(boton.text.toString())) {//y si has clickeado correctamente la pregunta
+
+                        comprobacion = true
+                        Toast.makeText(this, "Acertastes la pregunta", Toast.LENGTH_LONG).show()
+                        btnA.setBackgroundResource(R.drawable.button_round)
+                        btnB.setBackgroundResource(R.drawable.button_round)
+                        btnC.setBackgroundResource(R.drawable.button_round)
+                        btnD.setBackgroundResource(R.drawable.button_round)
+                        puntuacion+=5
+                        txtPuntuacionJugador.text = "Puntuacion: "+puntuacion
+
+                    }
+                }
+            }
+
+            if (comprobacion) {
+                comprobarPreguntas(preguntas)
+
+            } else {
+                contador--
+                txtVida.text = "Total de vidas:" + " " + contador
+                Toast.makeText(this, "Fallastes ", Toast.LENGTH_LONG).show()
+
+                //Ponemos de color rojo el boton de la pregunta
+                boton.setBackgroundResource(R.drawable.button_red)
+
+            }
+
+
+        }
+
+
+        if (this.contador == 0) {
+            terminarJuego()
+
+        }
+    }
+
+
+    fun puntuacionJugador(){
+
+
+        Database.firebaseDB.collection("usuarios").get().addOnSuccessListener { result ->
+
+            for (jugador in result) {
+
+                var jugador = jugador.toObject(Usuario::class.java) as Usuario
+                //Toast.makeText(this, ""+pregunta.id + result.size()+pregunta.descripcion, Toast.LENGTH_LONG).show()
+
+                if (jugador.email.equals(Database.firebaseAuth.currentUser.email)){
+                    this.jugador=jugador
+
+                }
+            }
+
+        }
+            .addOnFailureListener { exception ->
+                Toast.makeText(this, "" + exception, Toast.LENGTH_LONG).show()
+                //   Log.d(TAG, "Error getting documents: ", exception)
+            }
+
+
+    }
+
+
+    fun terminarJuego(){
+        jugador.ranking+=puntuacion
+        Database.firebaseDB.collection("usuarios").document(jugador.email.toString()).set(jugador).addOnCompleteListener(this,
+            OnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText( this,"Has conseguido de puntuacion "+puntuacion, Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText( this,"jugador no actualizado en la colleccion", Toast.LENGTH_LONG).show()
+                }
+            })
+        startActivity(Intent(this, Principal::class.java))
+
+    }
+
+
 
 
 }
